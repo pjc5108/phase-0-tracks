d4d3ae0a7f22c38bd3121a38f7a73fb88021f1c5
Philip Cornman <pjc5108@gmail.com>
2017-01-29 23:17:14 -0500

    Add loop based on how many employees get processed. Also has output to help debug.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,73 @@
-vamp = 0
 
-puts "What is your name?"
-emp_name = gets.chomp
+puts "How many employees will be processed?"
+num_emp = gets.to_i
 
-if emp_name == "Drake Cula" || emp_name == "Tu Fang"
-	vamp_name = true
-else 
-	vamp_name = false
-end
+num_emp.times {
 
-puts vamp_name
-puts vamp
+	vamp = 0
 
-puts "How old are you?"
-age = gets.to_i
+	puts "What is your name?"
+	emp_name = gets.chomp
 
-puts "What year were you born?"
-year_born = gets.to_i
-
-if age != 2017-year_born
-	vamp = vamp + 1
-else
-	vamp = vamp
-end
-
-puts vamp_name
-puts vamp
-
-puts "Our company cafeteria serves garlic bread. Should we get some for you?"
-gar_bread = gets.chomp
-
-if gar_bread == "no"
-	vamp = vamp + 1
-else 
-	vamp = vamp
-end
-
-puts vamp_name
-puts vamp
-
-puts "Would you like to enroll in our company's health insurance program?"
-insurance = gets.chomp
-
-if insurance == "no"
-	vamp = vamp + 1
-else
-	vamp = vamp
-end
-
-puts vamp_name
-puts vamp
-
-if vamp_name == true
-	puts "Definitely a vampire."
-else
-	if vamp == 1
-		puts "Probably not a vampire."
-	elsif vamp == 2
-		puts "Probably a vampire."
-	elsif vamp == 3
-		puts "Almost certainly a vampire."
-	else
-		puts "Results inconclusive."
+	if emp_name == "Drake Cula" || emp_name == "Tu Fang"
+		vamp_name = true
+	else 
+		vamp_name = false
 	end
-end
+
+	puts vamp_name
+	puts vamp
+
+	puts "How old are you?"
+	age = gets.to_i
+
+	puts "What year were you born?"
+	year_born = gets.to_i
+
+	if age != 2017-year_born
+		vamp = vamp + 1
+	else
+		vamp = vamp
+	end
+
+	puts vamp_name
+	puts vamp
+
+	puts "Our company cafeteria serves garlic bread. Should we get some for you?"
+	gar_bread = gets.chomp
+
+	if gar_bread == "no"
+		vamp = vamp + 1
+	else 
+		vamp = vamp
+	end
+
+	puts vamp_name
+	puts vamp
+
+	puts "Would you like to enroll in our company's health insurance program?"
+	insurance = gets.chomp
+
+	if insurance == "no"
+		vamp = vamp + 1
+	else
+		vamp = vamp
+	end
+
+	puts vamp_name
+	puts vamp
+
+	if vamp_name == true
+		puts "Definitely a vampire."
+	else
+		if vamp == 1
+			puts "Probably not a vampire."
+		elsif vamp == 2
+			puts "Probably a vampire."
+		elsif vamp == 3
+			puts "Almost certainly a vampire."
+		else
+			puts "Results inconclusive."
+		end
+	end
+}
