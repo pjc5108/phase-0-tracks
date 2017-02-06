5178aafb7c1081768e8d01f4912d314a86e75ad4
Philip Cornman <pjc5108@gmail.com>
2017-02-06 07:23:32 -0500

    finish release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,3 +14,24 @@ end
 
 test_array = [85, 22, 57, 34, 11]
 search_array(test_array, 11)
+
+
+def fib_gen(num)
+	fib_arr = [0, 1]
+	index_1 = 0
+	index_2 = 1
+	until index_2 == num -1
+		new_term = fib_arr[index_1] + fib_arr[index_2]
+		fib_arr.push(new_term)
+		index_1 += 1
+		index_2 += 1	
+	end
+	p fib_arr
+end
+
+fib_gen(100)
+if fib_gen(100).last == 218922995834555169026
+	puts "Your Fibonacci Generator works!"
+else
+	puts "You still have some work to do!"
+end
