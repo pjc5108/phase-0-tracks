0608fdf8aac49528ec5047b5d712c93b8f8982e4
Philip Cornman <pjc5108@gmail.com>
2017-02-12 10:19:40 -0500

    Finish release 0.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,38 @@ class Puppy
     toy
   end
 
+  def speak(num_barks)
+  	num_barks.times {
+  		puts "Woof!"
+  	}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years
+  	puts "How many human years are you converting to dog years?"
+  	human_years = gets.to_i
+  	pup_years = human_years * 7
+  	puts " #{human_years} human years is equal to #{pup_years} dog years!"
+  end
+
+  def handshake
+  	puts "*puppy puts its paw in your hand*"
+  end
+  
 end
 
+
+new_dog = Puppy.new
+
+new_dog.fetch("bone")
+
+new_dog.speak(5)
+
+new_dog.roll_over
+
+new_dog.dog_years
+
+new_dog.handshake
