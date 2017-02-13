70c18fbccf462816e62ecd86d5fcd8445fea2cef
Philip Cornman <pjc5108@gmail.com>
2017-02-13 10:22:42 -0500

    Progress on 6.6 challenge.

---
+++ ruby/my_game/game.rb
@@ -0,0 +1,21 @@
+# game class
+
+class WordGame
+	attr_accessor :game_word 
+	attr_reader :guess_counter :game_string
+
+	def initialize
+	end
+
+	def check_word(guess_letter, game_string)
+		@guess_counter -= 1
+
+
+	end
+
+
+
+
+end
+
+#obviously still working on this
