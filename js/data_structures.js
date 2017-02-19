3982f64e26b74e55ae9fc6b55560c03e543ce327
Philip Cornman <pjc5108@gmail.com>
2017-02-19 14:39:35 -0500

    Add a solution to create a hash using the two arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,15 @@
-var colors = ["red", "blue", "green", "orange"
+var colors = ["red", "blue", "green", "orange"]
 var names = ["Jeremy", "Beetus", "DiCaprio", "Armin"]
 
 colors.push("purple");
 names.push("Eleanor");
+
+horse_db = {}
+
+for (var i = 0; i < colors.length; i++) {
+	horse_db[names[i]] = colors[i];
+}
+
+
+console.log(horse_db)
+
